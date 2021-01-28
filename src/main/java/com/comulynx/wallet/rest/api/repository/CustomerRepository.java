b6d20c9fa5cb698cd20d9a5bf5f887e384deadd7
Shields10 <benjaminshields10@gmail.com>
2021-01-28 14:21:15 +0300

    Fixed all tasks in the project

---
+++ src/main/java/com/comulynx/wallet/rest/api/repository/CustomerRepository.java
@@ -0,0 +1,36 @@
+package com.comulynx.wallet.rest.api.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.comulynx.wallet.rest.api.model.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+	Optional<Customer> findByCustomerId(String customerId);
+
+	// TODO : Implement the query and function below to delete a customer using Customer Id
+	@Modifying 
+	@Query("DELETE from Customer WHERE customerId = :customerId")
+	 int deleteCustomerByCustomerId(@Param("customerId")String customer_id);
+	 
+
+	// TODO : Implement the query and function below to update customer firstName using Customer Id
+	 @Modifying
+	 @Query("UPDATE Customer u SET u.firstName = :firstName WHERE u.customerId = :customerId")
+	 int updateCustomerByCustomerId(@Param("firstName") String firstName, @Param("customerId")String customer_id);
+
+	
+	
+	// TODO : Implement the query and function below and to return all customers whose Email contains  'gmail'
+	 @Query("SELECT m FROM Customer m WHERE m.email LIKE ?1%")
+	 List<Customer> findAllCustomersWhoseEmailContainsGmail(String gmail);
+	 
+}
---
+++ src/main/java/com/comulynx/wallet/rest/api/repository/TransactionRepository.java
@@ -0,0 +1,35 @@
+package com.comulynx.wallet.rest.api.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.comulynx.wallet.rest.api.model.Transaction;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+	Optional<List<Transaction>> findTransactionsByCustomerId(String customerId);
+
+	Optional<List<Transaction>> findTransactionsByTransactionId(String transactionId);
+
+	Optional<List<Transaction>> findTransactionsByCustomerIdOrTransactionId(String transactionId, String customerId);
+
+	// TODO : Change below Query to return the last 5 transactions
+	// TODO : Change below Query to use Named Parameters instead of indexed
+	// parameters
+	// TODO : Change below function to return Optional<List<Transaction>>
+	@Query("SELECT t FROM Transaction t WHERE t.customerId =:customerid AND  t.accountNo = :accountno")
+	List<Transaction> getMiniStatementUsingCustomerIdAndAccountNo( @Param("customerid")String customer_id, @Param("accountno") String account_no);
+	
+	
+	
+
+	
+	
+	
+}
